e122f38969d91b75555d7b6cfd369bc391af5be5
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2025-04-30 14:28:17 +0530

    ALLOWED_HOSTS

--- image_steganography/stego_project/settings.py
+++ image_steganography/stego_project/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'steganography-6uy9.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
